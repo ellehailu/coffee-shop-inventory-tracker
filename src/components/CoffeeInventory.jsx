e221d49a8890acd72773a8268fba6a2e8994bebd
ellehailu <hailu.elle@gmail.com>
2023-09-09 12:00:23 -0700

    update coffee control component

--- src/components/CoffeeInventory.jsx
+++ src/components/CoffeeInventory.jsx
@@ -2,27 +2,6 @@ import React from "react";
 import Coffee from "./Coffee";
 import PropTypes from "prop-types";
 
-// function CoffeeInventory(props){
-//     return(
-//         <React.Fragment>
-//             {props.coffeeList.map((coffee) =>
-//             <Coffee
-//                 whenCoffeeClicked = { props.onCoffeeSelection }
-//                 type={coffee.type}
-//                 origin={coffee.origin}
-//                 roast= {coffee.roast}
-//                 price= {coffee.price}
-//                 id = {coffee.id}
-//                 key = {coffee.id} />
-//             )}
-//         </React.Fragment>
-//     );
-// }
-
-// CoffeeInventory.propTypes = {
-//     coffeeList: PropTypes.array,
-//     onCoffeeSelection: PropTypes.func
-// }
 
 function CoffeeInventory(){
     return(
