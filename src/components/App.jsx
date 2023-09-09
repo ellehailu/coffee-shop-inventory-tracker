91717cf89456dac7f11705505203de50999d8f23
ellehailu <hailu.elle@gmail.com>
2023-09-09 10:59:41 -0700

    update coffee inventory component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-      {/* <CoffeeInventory /> */}
+      <CoffeeInventory />
     </React.Fragment>
   );
 }
--- src/components/CoffeeInventory.jsx
+++ src/components/CoffeeInventory.jsx
@@ -2,26 +2,31 @@ import React from "react";
 import Coffee from "./Coffee";
 import PropTypes from "prop-types";
 
-function CoffeeInventory(props){
+// function CoffeeInventory(props){
+//     return(
+//         <React.Fragment>
+//             {props.coffeeList.map((coffee) =>
+//             <Coffee
+//                 whenCoffeeClicked = { props.onCoffeeSelection }
+//                 type={coffee.type}
+//                 origin={coffee.origin}
+//                 roast= {coffee.roast}
+//                 price= {coffee.price}
+//                 id = {coffee.id}
+//                 key = {coffee.id} />
+//             )}
+//         </React.Fragment>
+//     );
+// }
+
+// CoffeeInventory.propTypes = {
+//     coffeeList: PropTypes.array,
+//     onCoffeeSelection: PropTypes.func
+// }
+
+function CoffeeInventory(){
     return(
-        <React.Fragment>
-            {props.coffeeList.map((coffee) =>
-            <Coffee
-                whenCoffeeClicked = { props.onCoffeeSelection }
-                type={coffee.type}
-                origin={coffee.origin}
-                roast= {coffee.roast}
-                price= {coffee.price}
-                id = {coffee.id}
-                key = {coffee.id} />
-            )}
-        </React.Fragment>
+        <Coffee />
     );
 }
-
-CoffeeInventory.propTypes = {
-    coffeeList: PropTypes.array,
-    onCoffeeSelection: PropTypes.func
-}
-
 export default CoffeeInventory;
