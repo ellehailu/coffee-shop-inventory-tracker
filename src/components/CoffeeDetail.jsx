03b1aa21a692eac7a4cfeeaf1188a2ab4daaef26
ellehailu <hailu.elle@gmail.com>
2023-09-12 10:03:19 -0700

    add sell coffee button

--- src/components/CoffeeDetail.jsx
+++ src/components/CoffeeDetail.jsx
@@ -7,14 +7,15 @@ function CoffeeDetail(props){
     return (
         <React.Fragment>
             <h1>Coffee Detail</h1>
-            <h3>{coffee.type}</h3>
-            <h3>{coffee.name}</h3>
-            <h4>{coffee.origin}</h4>
-            <h4>{coffee.roast}</h4>
-            <h4>{coffee.price}</h4>
-            <h4>{coffee.amountAvailable}</h4>
+            <h3>Type: {coffee.type}</h3>
+            <h3>Name: {coffee.name}</h3>
+            <h4>Origin: {coffee.origin}</h4>
+            <h4>Roast: {coffee.roast}</h4>
+            <h4>Price: ${coffee.price}</h4>
+            <h4>Amount available: {coffee.amountAvailable}lbs</h4>
             <button onClick={props.onClickingEdit }>Update Coffee</button>
             <button onClick={() => onClickingDelete(coffee.id)}>Delete Coffee</button>
+            <button onClick={coffee.amountAvailable - 1}>sell coffee</button>
             <hr/>
         </React.Fragment>
     )
@@ -24,6 +25,7 @@ CoffeeDetail.propTypes = {
     coffee: PropTypes.object,
     onClickingDelete: PropTypes.func,
     onClickingEdit: PropTypes.func
+
 };
 
 export default CoffeeDetail;
